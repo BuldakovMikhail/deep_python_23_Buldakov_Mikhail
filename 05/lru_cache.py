874315d23101a68213e90b973cc214c2413fc9d8
Buldakov Mikhail <mishabul2003@gmail.com>
2023-10-26 00:00:40 +0300

    05 done

---
+++ 05/lru_cache.py
@@ -0,0 +1,69 @@
+class ListNode:
+    def __init__(self, key=None, val=None):
+        self.val = val
+        self.key = key
+        self.next = None
+        self.prev = None
+
+
+class LRUCache:
+    def __init__(self, limit=42):
+        if limit <= 0:
+            raise ValueError("Limit must be greater than zero")
+
+        self.cache = {}
+        self.head = None
+        self.tail = None
+        self.limit = limit
+
+    def __delete_node(self, node):
+        if node == self.tail:
+            self.tail = node.next
+            if self.tail:
+                self.tail.prev = None
+
+        elif node == self.head:
+            self.head = node.prev
+            if self.head:
+                self.head.next = None
+
+        else:
+            if node.prev:
+                node.prev.next = node.next
+            if node.next:
+                node.next.prev = node.prev
+
+    def __add_node(self, node):
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else:
+            self.head.next = node
+            node.prev = self.head
+            self.head = node
+
+    def get(self, key):
+        if key in self.cache:
+            temp = self.cache[key]
+            self.__delete_node(temp)
+            self.__add_node(temp)
+
+            return temp.val
+
+        return None
+
+    def set(self, key, value):
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+
+            self.__delete_node(node)
+            self.__add_node(node)
+        else:
+            if len(self.cache) >= self.limit:
+                self.cache.pop(self.tail.key)
+                self.__delete_node(self.tail)
+
+            new_node = ListNode(key, value)
+            self.__add_node(new_node)
+            self.cache[key] = new_node
---
+++ 05/test_lru.py
@@ -0,0 +1,61 @@
+import unittest
+
+from lru_cache import LRUCache
+
+
+class TestLRUCache(unittest.TestCase):
+    def test_lru_remove_least_on_add(self):
+        cache = LRUCache(2)
+
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+
+        self.assertIsNone(cache.get("k3"))
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k1"), "val1")
+
+        cache.set("k3", "val3")
+
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertIsNone(cache.get("k2"))
+        self.assertEqual(cache.get("k1"), "val1")
+
+    def test_lru_value_update(self):
+        cache = LRUCache(2)
+
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+
+        self.assertIsNone(cache.get("k3"))
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k1"), "val1")
+
+        cache.set("k1", "val1_1")
+        self.assertIsNone(cache.get("k3"))
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k1"), "val1_1")
+
+        cache.set("k3", "val3")
+
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertIsNone(cache.get("k2"))
+        self.assertEqual(cache.get("k1"), "val1_1")
+
+    def test_lru_value_check_addition_with_out_get(self):
+        cache = LRUCache(2)
+
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+        cache.set("k3", "val3")
+
+        self.assertIsNone(cache.get("k1"))
+
+    def test_lru_get_from_empty(self):
+        cache = LRUCache(2)
+
+        self.assertIsNone(cache.get("k1"))
+
+    def test_create_cache_with_size_0(self):
+        with self.assertRaises(ValueError) as context:
+            LRUCache(0)
+        self.assertTrue("greater than zero" in str(context.exception))
