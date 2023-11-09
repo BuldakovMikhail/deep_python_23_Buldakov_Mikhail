38da4261445bb3635ff3e231317fe1e87a1975ea
Buldakov Mikhail <mishabul2003@gmail.com>
2023-11-09 15:08:50 +0300

    07 done

---
+++ 07/fetcher.py
@@ -0,0 +1,75 @@
+import aiohttp
+import asyncio as aio
+
+from bs4 import BeautifulSoup
+from collections import Counter
+
+import sys
+
+
+def get_most_common_words(resp, keys_count=5):
+    soup = BeautifulSoup(resp, "html.parser")
+    text = soup.get_text().split()
+    temp = dict(Counter(text).most_common(keys_count))
+    return temp
+
+
+async def fetch_url(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            text = await resp.read()
+            return text
+
+
+async def fetch_worker(que, answers_collecter):
+    while True:
+        url = await que.get()
+        try:
+            resp = await fetch_url(url)
+
+            th = aio.create_task(aio.to_thread(get_most_common_words, resp))
+            await th
+
+            # Тут собираем ответы в один коллектор,
+            # чтобы его вернуть пользователю, но как вариант,
+            # тут можно было прописать print(...)
+            # или какой-то другой вывод информации
+            print(f"{url=}: {th.result()}")
+            answers_collecter.append((url, th.result()))
+
+        finally:
+            que.task_done()
+
+
+async def batch_fetch(fname, workers_count):
+    que = aio.Queue(workers_count)
+    answers = []
+
+    workers = [
+        aio.create_task(fetch_worker(que, answers)) for _ in range(workers_count)
+    ]
+
+    with open(fname, "r", encoding="utf-8") as src:
+        while url := src.readline():
+            await que.put(url)
+    await que.join()
+
+    for worker in workers:
+        worker.cancel()
+
+    return answers
+
+
+def parse_argv():
+    workers_count = sys.argv[sys.argv.index("-c") + 1]
+    file_name = sys.argv[-1]
+    return int(workers_count), file_name
+
+
+if __name__ == "__main__":
+    wc, fn = parse_argv()
+
+    loop = aio.new_event_loop()
+    aio.set_event_loop(loop)
+
+    loop.run_until_complete(batch_fetch(fn, wc))
---
+++ 07/test_fetcher.py
@@ -0,0 +1,146 @@
+from fetcher import get_most_common_words, fetch_url, batch_fetch
+
+from unittest import IsolatedAsyncioTestCase
+from unittest import mock
+
+import io
+import contextlib
+
+
+class TestFetcher(IsolatedAsyncioTestCase):
+    def test_most_common_words(self):
+        resp = "<html> 1 2 2 3 3 , 3 , 4 4 4 4 5 5 5 5 5 6 6 6 6 6 6 </html>"
+        most_common = get_most_common_words(resp, 2)
+
+        self.assertDictEqual(most_common, {"6": 6, "5": 5})
+
+    def test_most_common_words_on_letters(self):
+        resp = "<html> a b b c c c d d d d e e e e e f f f f f f </html>"
+        most_common = get_most_common_words(resp, 3)
+
+        self.assertDictEqual(most_common, {"f": 6, "e": 5, "d": 4})
+
+    @mock.patch("aiohttp.ClientSession.get")
+    async def test_fetch_url(self, mock_get):
+        mock_get.return_value.__aenter__.return_value.read.return_value = "test text"
+
+        val = await fetch_url("test url")
+
+        self.assertEqual(val, "test text")
+        self.assertTrue(mock.call("test url") in mock_get.mock_calls)
+
+    @mock.patch("aiohttp.ClientSession.get")
+    async def test_batch_fetch_one_worker(self, mock_get):
+        mock_get.return_value.__aenter__.return_value.read.return_value = (
+            "<html> a b b c c c d d d d e e e e e f f f f f f </html>"
+        )
+
+        lines = ["tu1", "tu2", "tu3\n"]
+        text = "\n".join(lines)
+
+        mock_open = mock.AsyncMock()
+        mock_open.return_value.__aenter__.return_value.readline = "test url"
+
+        res = [
+            ("tu1\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+            ("tu2\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+            ("tu3\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+        ]
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            with mock.patch(
+                "builtins.open", new_callable=mock.mock_open, read_data=text
+            ):
+                val = await batch_fetch("aboba", 1)
+
+                self.assertEqual(val, res)
+                self.assertTrue(mock.call("tu1\n") in mock_get.mock_calls)
+                self.assertTrue(mock.call("tu2\n") in mock_get.mock_calls)
+                self.assertTrue(mock.call("tu3\n") in mock_get.mock_calls)
+
+    @mock.patch("aiohttp.ClientSession.get")
+    async def test_batch_fetch_workers_count_equal_lines(self, mock_get):
+        mock_get.return_value.__aenter__.return_value.read.return_value = (
+            "<html> a b b c c c d d d d e e e e e f f f f f f </html>"
+        )
+
+        lines = ["tu1", "tu2", "tu3\n"]
+        text = "\n".join(lines)
+
+        mock_open = mock.AsyncMock()
+        mock_open.return_value.__aenter__.return_value.readline = "test url"
+
+        res = [
+            ("tu1\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+            ("tu2\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+            ("tu3\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+        ]
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            with mock.patch(
+                "builtins.open", new_callable=mock.mock_open, read_data=text
+            ):
+                val = await batch_fetch("aboba", 3)
+
+                self.assertEqual(val, res)
+                self.assertTrue(mock.call("tu1\n") in mock_get.mock_calls)
+                self.assertTrue(mock.call("tu2\n") in mock_get.mock_calls)
+                self.assertTrue(mock.call("tu3\n") in mock_get.mock_calls)
+
+    @mock.patch("aiohttp.ClientSession.get")
+    async def test_batch_fetch_more_workers_that_lines(self, mock_get):
+        mock_get.return_value.__aenter__.return_value.read.return_value = (
+            "<html> a b b c c c d d d d e e e e e f f f f f f </html>"
+        )
+
+        lines = ["tu1", "tu2", "tu3\n"]
+        text = "\n".join(lines)
+
+        mock_open = mock.AsyncMock()
+        mock_open.return_value.__aenter__.return_value.readline = "test url"
+
+        res = [
+            ("tu1\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+            ("tu2\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+            ("tu3\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+        ]
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            with mock.patch(
+                "builtins.open", new_callable=mock.mock_open, read_data=text
+            ):
+                val = await batch_fetch("aboba", 10)
+
+                self.assertEqual(val, res)
+                self.assertTrue(mock.call("tu1\n") in mock_get.mock_calls)
+                self.assertTrue(mock.call("tu2\n") in mock_get.mock_calls)
+                self.assertTrue(mock.call("tu3\n") in mock_get.mock_calls)
+
+    @mock.patch("aiohttp.ClientSession.get")
+    async def test_batch_fetch_less_workers_than_lines(self, mock_get):
+        mock_get.return_value.__aenter__.return_value.read.return_value = (
+            "<html> a b b c c c d d d d e e e e e f f f f f f </html>"
+        )
+
+        lines = ["tu1", "tu2", "tu3\n"]
+        text = "\n".join(lines)
+
+        mock_open = mock.AsyncMock()
+        mock_open.return_value.__aenter__.return_value.readline = "test url"
+
+        res = [
+            ("tu1\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+            ("tu2\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+            ("tu3\n", {"f": 6, "e": 5, "d": 4, "c": 3, "b": 2}),
+        ]
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            with mock.patch(
+                "builtins.open", new_callable=mock.mock_open, read_data=text
+            ):
+                val = await batch_fetch("aboba", 2)
+
+                self.assertEqual(val, res)
+                self.assertTrue(mock.call("tu1\n") in mock_get.mock_calls)
+                self.assertTrue(mock.call("tu2\n") in mock_get.mock_calls)
+                self.assertTrue(mock.call("tu3\n") in mock_get.mock_calls)
